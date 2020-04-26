e0fa3fceb001f158cdf5f1f9564b881d99b2d9d7
niujianqun <niujianqun@126.com>
2020-04-26 22:44:05 +0800

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='szhyy_exam',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='Administrator',
+    author_email='niujianqun@126.com',
+    description=''
+)
